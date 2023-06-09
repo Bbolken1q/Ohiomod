d13e947dce50ec86b02cf45a5bb6e72bea526938
Bbolken1q <agent63.yt@gmail.com>
2023-06-09 15:08:10 +0200

    ''

--- src/main/java/com/BolekB/ohiomod/item/ModItems.java
+++ src/main/java/com/BolekB/ohiomod/item/ModItems.java
@@ -8,20 +8,19 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import static com.BolekB.ohiomod.Ohiomod.MOD_ID;
-
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Ohiomod.MOD_ID);
 
-    private static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("ohium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
-    public static final RegistryObject<Item> OHIUM = ITEMS.register("ohium",
+    public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_ohium",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
 
 
-    public static void register(IEventBus eventBus){
-        ITEMS.register((eventBus));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
     }
-
 }
