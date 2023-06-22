f6c8a8c9f12c8962db7f6628b435adc6a3e3fd4c
Bbolken1q <agent63.yt@gmail.com>
2023-06-22 17:22:05 +0200

    Baller Armor

--- src/main/java/com/BolekB/ohiomod/item/ModItems.java
+++ src/main/java/com/BolekB/ohiomod/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.BolekB.ohiomod.item;
 
 import com.BolekB.ohiomod.Ohiomod;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,11 +14,20 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Ohiomod.MOD_ID);
 
-    public static final RegistryObject<Item> CITRINE = ITEMS.register("ohium",
+    public static final RegistryObject<Item> OHIUM = ITEMS.register("ohium",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
 
-    public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_ohium",
+    public static final RegistryObject<Item> RAW_OHIUM = ITEMS.register("raw_ohium",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+/*
+    Baller Armor
+*/
+    public static final RegistryObject<Item> BALLER_SNEAKERS = ITEMS.register("baller_sneakers",
+            () -> new ArmorItem(ModArmorMaterials.OHIUM, EquipmentSlot.FEET, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
+    public static final RegistryObject<Item> BALLER_PANTS = ITEMS.register("baller_pants",
+            () -> new ArmorItem(ModArmorMaterials.OHIUM, EquipmentSlot.LEGS, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
 
 
 
---
+++ src/main/java/com/BolekB/ohiomod/item/custom/BallGunItem.java
@@ -0,0 +1,13 @@
+/*
+package com.BolekB.ohiomod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+
+public class BallGunItem extends Item{
+    public BallGunItem(Properties pProperties) {
+        super(pProperties);
+    }
+    override
+}
+*/
